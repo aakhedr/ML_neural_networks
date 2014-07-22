ec005d685711ebf017bdb11a7f671b75e2378a6a
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2014-07-22 12:30:07 +0200

    _

--- predictOneVsAll.m
+++ predictOneVsAll.m
@@ -7,16 +7,12 @@ function p = predictOneVsAll(all_theta, X)
 %  regression theta vector for the i-th class. You should set p to a vector
 %  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
 %  for 4 examples) 
-
 m = size(X, 1);
 num_labels = size(all_theta, 1);
-
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
-
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned logistic regression parameters (one-vs-all).
@@ -28,15 +24,6 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
-
-
-
-
-
-
-
+%
 % =========================================================================
-
-
 end
